dbd4be96d80c69955f1992d30574d4a98abbd002
groomigroom <googjohn0143@gmail.com>
2026-05-15 16:39:56 +0900

    Update C_pointer.c

--- my_test/C_language/C_pointer.c
+++ my_test/C_language/C_pointer.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 int main()
 {
+  /*
   int *p;
   *p = 10;
   printf("%d", *p);
+  위가 같이 하면 안됨...??
+  */
   //이렇게 주소를 저장할 값 출력??
+  int a;
+  int *p;
+  
+  p = &a;
+  *p = 10;
+  
+  printf("%d", *p);
+  
+  int s[4] = {1, 2, 3};
+  return 0;
 }
