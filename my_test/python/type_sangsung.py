e0fed772c9582030132c64a72f239fab4fcaf7af
groomigroom <googjohn0143@gmail.com>
2026-05-15 17:32:38 +0900

    Add function to determine battle outcomes

---
+++ my_test/python/type_sangsung.py
@@ -0,0 +1,58 @@
+def sangsung (st_mytype, st_juktype) : 
+  global win, lose, same
+
+  if st_mytype == "불꽃" :
+    if st_juktype == "불꽃" or st_juktype == "물" or st_juktype == "얼음" :
+      same += 1
+    elif st_juktype == "풀" :
+      win += 1
+    else :
+      lose += 1
+  
+  elif st_mytype == "전기" :
+    if st_juktype == "전기" or st_juktype == "얼음" or st_juktype == "풀" :
+      same += 1
+    elif st_juktype == "불꽃" :
+      win += 1
+    else :
+      lose += 1
+
+  elif st_mytype == "물" :
+    if st_juktype == "물" or st_juktype == "풀" or st_juktype == "불꽃" :
+      same += 1
+    elif st_juktype == "전기" :
+      win += 1
+    else :
+      lose += 1
+  
+  elif st_mytype == "얼음" :
+    if st_juktype == "얼음" or st_juktype == "전기" or st_juktype == "불꽃" :
+      same += 1
+    elif st_juktype == "물" :
+      win += 1
+    else :
+      lose += 1
+
+  else :
+    if st_juktype == "풀" or st_juktype == "전기" or st_juktype == "물" :
+      same += 1
+    elif st_juktype == "얼음" :
+      win += 1
+    else :
+      lose += 1
+
+  return win, lose, same
+
+win = 0
+same = 0
+lose = 0
+
+sangsung ("풀", "얼음")
+sangsung ("불꽃", "물")
+sangsung ("불꽃", "풀")
+
+  
+print(f"승: {win}, 패: {lose}, 무승부: {same}회입니다.")
+
+
+#불꽃->전기->물->얼음->풀->불꽃
