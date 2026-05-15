2a15f6e9440281cdcf9ba55bb919fac239b4d804
groomigroom <googjohn0143@gmail.com>
2026-05-15 17:14:55 +0900

    Update pointer_ex1.c

--- my_test/C_language/pointer/pointer_ex1.c
+++ my_test/C_language/pointer/pointer_ex1.c
@@ -7,4 +7,10 @@ struct student {
 
 int main() {
   struct student s[3] = {{"김구름", 80, 92}, {"김구구", 72, 99}, {"김구루움", 12, 44}};
+  struct student *p;
+  p = &s[0];
+  
+  (p + 1) -> sum = (p + 1) -> kor + (p + 2) -> math;
+  (p + 1) -> total = (p + 1) -> sum + p -> kor + p -> math;
+  
 }
