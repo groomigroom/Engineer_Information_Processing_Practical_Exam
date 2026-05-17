89708e70d4a3b613bd4badd9d8095d526c9dce2b
groomigroom <googjohn0143@gmail.com>
2026-05-17 21:12:31 +0900

    Create group_by_with_rollup.sql

---
+++ my_test/sql/group_by_with_rollup.sql
@@ -0,0 +1,21 @@
+create table groomi
+(
+  groo_id integer,
+  groo_name varchar(50),
+  groo_part varchar(50),
+  groo_id_two integer
+);
+
+desc groomi;
+
+insert into groomi
+values
+(1, "구름이", "1부", 2),
+(2, "구미", "2부", 3),
+(3, "구구미", "3부", 4),
+(4, "구우", "2부", 5);
+
+select groo_part, sum(groo_id_two) id_two_hap
+from groomi
+group by groo_part with rollup;
+-- with rollup으로 총계도 구하는거
