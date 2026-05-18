77d55a3c72644db5d454946717f310d061157bb0
groomigroom <googjohn0143@gmail.com>
2026-05-18 12:14:43 +0900

    Create outer_join.sql

---
+++ my_test/sql/outer_join.sql
@@ -0,0 +1,31 @@
+create table groom (
+  col1 integer,
+  col2 varchar(50)
+);
+
+insert into groom
+values
+(1, "구름"),
+(2, "김구름"),
+(3, "김구구름"),
+(4, "김름름"),
+(6, "김구구름름");
+
+create table groom2 (
+  col1 integer,
+  col2 varchar(50)
+);
+
+insert into groom2
+values
+(1, "groom"),
+(2, "kimgroom"),
+(3, "kimgoogooroom"),
+(4, "kimgromroom"),
+(5, "gugu");
+
+select groom.col1, groom2.col1, groom.col2, groom2.col2
+from groom
+left join groom2
+-- left outer join groom2도 가능함.
+on groom.col1 = groom2.col1;
