a4be56af7aff6b01ebffe017bd4939cbad74b53e
groomigroom <googjohn0143@gmail.com>
2026-05-18 11:50:55 +0900

    Create inner_join.sql

---
+++ my_test/sql/inner_join.sql
@@ -0,0 +1,30 @@
+create table groom (
+  col1 integer,
+  col2 varchar(50)
+);
+
+insert into groom
+values
+(1, "구름"),
+(2, "김구름"),
+(3, "김구구름"),
+(4, "김름름");
+
+create table groom2 (
+  col1 integer,
+  col2 varchar(50)
+);
+
+insert into groom2
+values
+(1, "groom"),
+(2, "kimgroom"),
+(3, "kimgoogooroom"),
+(4, "kimgromroom"),
+(5, "gugu");
+
+select groom.col1, groom.col2, groom2.col2
+from groom
+join groom2
+-- inner join groom2로도 가능함.
+on groom.col1 = groom2.col1;
