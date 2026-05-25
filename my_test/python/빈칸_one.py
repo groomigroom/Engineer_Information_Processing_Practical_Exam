40960e5026026e443a6d8bee4820c4898029550f
groomigroom <googjohn0143@gmail.com>
2026-05-25 22:25:15 +0900

    Create 빈칸_one.py

---
+++ my_test/python/빈칸_one.py
@@ -0,0 +1,19 @@
+a = 0
+sum = 0
+for i in range (0, 8, 1) :
+    if ________ : //빈칸채우기 
+        continue
+    sum += i
+print(sum)
+#16
+"""
+a = 0
+sum = 0
+for i in range (0, 8, 1) :
+    if i % 2 == 0 : #여기 빈칸으로
+        continue
+    sum += i
+    #1 3 5 7 다 더하면, 16
+print(sum)
+#16
+"""
